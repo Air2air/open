50b8b5dc1983fd9e2bcc84c4d36bf680367e80ed
Air2air <todd@dunningmarketing.com>
2022-10-03 14:23:00 -0700

    Content section

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import React from "react";
-import {
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
 import ContactPage from "./pages/ContactPage/ContactPage";
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Container>
+      <BubbleSection>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </BrowserRouter>
+      </BubbleSection>
+      <ContentSection></ContentSection>
+    </Container>
   );
 }
+
+const Container = styled.div`
+display:flex;
+width:100%;
+height;100vy;
+`;
+
+const BubbleSection = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100vy;
+`;
+
+const ContentSection = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100vy;
+`;
