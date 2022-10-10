c2d88835d771677dd30ab5cf735297a468c82181
Air2air <todd@dunningmarketing.com>
2022-10-10 12:18:38 -0700

    Data cleanup

---
+++ src/components/Graph/config.tsx
@@ -0,0 +1,26 @@
+// Sizes
+export const NodeSize = 10;
+export const SiteNodeSize = NodeSize;
+export const LevelOneNodeSize = NodeSize * 0.7;
+export const LevelTwoNodeSize = NodeSize * 0.4;
+export const LinkSize = 5;
+export const SiteLinkSize = LinkSize;
+export const LevelOneLinkSize = LinkSize * 0.8;
+export const LevelTwoLinkSize = LevelOneLinkSize * 0.8;
+
+// Colors
+export const BioColorNode = "rgba(255, 123, 0, 1)";
+export const BioColorLink = "rgba(255, 123, 0, .3)";
+export const HealthColorNode = "rgba(71, 145, 214, 1)";
+export const HealthColorLink = "rgba(71, 145, 214, .3)";
+export const MedColorNode = "rgba(68, 173, 161, 1)";
+export const MedColorLink = "rgba(68, 173, 161, .3)";
+export const PharmaColorNode = "rgba(240, 53, 2, 1)";
+export const PharmaColorLink = "rgba(240, 53, 2, .3)";
+export const SiteColorNode = "rgba(171, 242, 255, 1)";
+export const SiteColorLink = "rgba(171, 242, 255, .3)";
+
+// Other
+export const zoomLevel = 1.5
+export const particleCount = 8;
+export const ParticleSize = 4;
