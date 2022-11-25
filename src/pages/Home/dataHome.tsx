7178c9b2f2b1dc43af56c7c3fd885acf6452ab9a
Air2air <todd@dunningmarketing.com>
2022-11-25 11:14:40 -0800

    Mobile fixes

--- src/pages/Home/dataHome.tsx
+++ src/pages/Home/dataHome.tsx
@@ -2,7 +2,7 @@ export const dataHome = [
   {
     id: 1,
     title: "We know what you need",
-    text: "The most experienced healthcare and clinical machine learning and business leaders from leading academic medical centers, venture capital, large technology companies, and successful digital health startups.",
+    text: "<p>The most experienced healthcare and clinical machine learning and business leaders from leading academic medical centers, venture capital, large technology companies, and successful digital health startups.</p>",
     side: "left",
     backgroundColor: 1,
     buttonText: "What We Do",
