e54efae9e7996ad4c1999cdcd858b1b5f94886a8
Air2air <todd@dunningmarketing.com>
2022-12-05 14:25:08 -0800

    Dashboard

--- src/pages/PracticeAreas/dataPracticeAreas.tsx
+++ src/pages/PracticeAreas/dataPracticeAreas.tsx
@@ -2,6 +2,7 @@ export const dataPracticeAreas = [
   {
     id: 0,
     title: "Clinical",
+    label: "Clinical",
     text: "The world's foremost clinical talent guides Eleven's digital health ventures to the best possible outcomes. Your CXAIO offers deep technical and clinical support for broader consumer health sector landscape (e.g., patient engagement, wearables, digital health and wellness).",
     color: "#FFFFFF",
     backgroundColor: "#858457",
@@ -9,6 +10,7 @@ export const dataPracticeAreas = [
   {
     id: 1,
     title: "AI/ML",
+    label: "AI/ML",
     text: "Experts from the preeminent academic and commercial AI/ML programs accelerate Eleven's digital health ventures.",
     color: "#FFFFFF",
     backgroundColor: "#6d825c",
@@ -16,6 +18,7 @@ export const dataPracticeAreas = [
   {
     id: 2,
     title: "Product",
+    label: "Product",
     text: "Commercial product specialists from leading commercial enterprises help sculpt Eleven's digital health ventures, including: <p><ul><li>Go-to-market and product positioning.</li><li>Pricing methodology for clinical machine learning and digital health solutions.</li><li>Segment landscape, needs, and priorities to drive adoption.</li></ul></p>",
     color: "#FFFFFF",
     backgroundColor: "#557f66",
@@ -23,6 +26,7 @@ export const dataPracticeAreas = [
   {
     id: 4,
     title: "Marketing",
+    label: "Marketing",
     text: "Professionals shaping some of the most storied companies in modern healthcare position Eleven's digital health ventures.  This can also include support for early adoption academic initiatives and activities including grants, publications, and collaborations.",
     color: "#FFFFFF",
     backgroundColor: "#45737e",
@@ -30,6 +34,7 @@ export const dataPracticeAreas = [
   {
     id: 5,
     title: "Regulatory",
+    label: "Regulatory",
     color: "#FFFFFF",
     text: "Authorities in FDA regulation and practice provide support for Eleven's digital health ventures.",
     backgroundColor: "#416283",
@@ -37,6 +42,7 @@ export const dataPracticeAreas = [
   {
     id: 6,
     title: "Strategy",
+    label: "Strategy",
     color: "#FFFFFF",
     text: "Strategists responsible for inventing and innovating new market segments over two decades fabricate direction for Eleven's digital health ventures. These can include:<p><ul><li>Strategic investments and structuring corporate vehicles (e.g., corporate venture capital, or CVC).</li><li>Growth guidance including market segment, therapeutic area or disease category from competitive, partnering or investment perspectives.</li><li>Multidisciplinary fundraising, positioning vision, strategy and business model for digital health and digital medicine.</li><li> M & A commercial due diligence including market assessment, evaluation of the competitive landscape, and analysis of the target's value proposition and post-acquisition growth strategy.</ul></p>",
     backgroundColor: "#37486e",
