287ab49279284bb4ace75ebe52dea48e149fa4d5
Air2air <todd@dunningmarketing.com>
2022-12-25 17:31:30 -0800

    Layout

--- src/components/Heading/headingHistory.tsx
+++ src/components/Heading/headingHistory.tsx
@@ -1,6 +1,5 @@
 import { TitleHeading } from "components/Title/titleHeading";
 import { SubHeadHeading } from "components/SubHead/subHeadHeading";
-import { useEffect, useState } from "react";
 import styled from "styled-components";
 import {
   BREAKPOINT,
@@ -8,47 +7,16 @@ import {
   HEADING_HEIGHT_DESKTOP,
   HEADING_HEIGHT_MOBILE,
 } from "constants/index";
-
 import Container from "components/Container/container";
 
-const LoopEndTime = 10000;
-
 const HeadingHistory = (props: { backgroundColor?: any }) => {
-  const [textHistoryOpacity, setTextHistoryOpacity] = useState(0);
-
-  const [count, setCount] = useState(1);
-
-  useEffect(() => {
-    setTextHistoryOpacity(0);
-
-    const textHistoryStart = setTimeout(() => {
-      setTextHistoryOpacity(1);
-    }, 1000);
-
-    const textHistoryEnd = setTimeout(() => {
-      setTextHistoryOpacity(0);
-    }, 5000);
-
-    const loopIsOver = setTimeout(() => {
-      setCount(count + 1);
-    }, LoopEndTime);
-
-    return () => {
-      clearTimeout(textHistoryStart);
-      clearTimeout(textHistoryEnd);
-
-      clearTimeout(loopIsOver);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [count]);
-
   return (
     <>
       <Container backgroundColor={props.backgroundColor}>
         <Wrapper>
-          <TextSection style={{ opacity: textHistoryOpacity }}>
-            <TitleHeading title="Healthcare AI History" />
-            <SubHeadHeading text="Elevens supports Healthtech ventures from early private stages through initial public offering (IPO), and assists public companies to identify innovative private sector partners" />
+          <TextSection>
+            <TitleHeading title="Elevens History" />
+            <SubHeadHeading text="" />
           </TextSection>
         </Wrapper>
       </Container>
@@ -58,7 +26,7 @@ const HeadingHistory = (props: { backgroundColor?: any }) => {
 
 const Wrapper = styled.div`
   position: relative;
-  top: 0;
+  height: 100%;
   @media (min-width: ${BREAKPOINT}px) {
     height: ${HEADING_HEIGHT_DESKTOP}px;
   }
@@ -73,6 +41,7 @@ const TextSection = styled.div`
   top: 0;
   transition: all ${CHART_TEXT_TRANSITION}ms;
   width: 100%;
+  height: 100%;
   @media (min-width: ${BREAKPOINT}px) {
     min-height: ${HEADING_HEIGHT_DESKTOP}px;
   }
