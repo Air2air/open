d8d28ae7c5eaea71c12464805e88408eba6be601
Air2air <todd@dunningmarketing.com>
2022-12-28 12:16:26 -0800

    Layout

--- src/pages/Home/pageHome.tsx
+++ src/pages/Home/pageHome.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "react-query";
 import OrgsCallout from "../../components/OrgsCallout/orgsCallOut";
 import ChartHome from "components/Chart/Home/chartHome";
 import RowButton from "components/Button/rowButton";
@@ -5,30 +6,60 @@ import BannerVideo from "components/BannerVideo/bannerVideo";
 import { COLOR_BASE } from "constants/index";
 import { dataHome } from "./dataHome";
 
+const dataSource = "data/dataHome.json";
+const queryName = "home";
+
 const HomePage = () => {
+  const fetchData = async () => {
+    const res = await fetch(dataSource, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    return res.json();
+  };
+  const { data, status } = useQuery(queryName, fetchData);
+
   const loopEndTime = 14000;
 
   return (
     <>
-      <BannerVideo
-        text="home"
-        pageVideo={776443340}
-        overlayOpacity={0.1}
-        overlayColor={COLOR_BASE}
-        loopEndTime={loopEndTime}
-      />
-      <ChartHome
-        columnCount={7}
-        data={dataHome}
-        loopEndTime={loopEndTime}
-        backgroundColor={1}
-      />
-      <RowButton
-        buttonTo="/practice_areas"
-        buttonText="What we do"
-        backgroundColor={4}
-      />
-      <OrgsCallout />
+      {status === "error" && (
+        <p style={{ color: "red" }}>Error fetching data</p>
+      )}
+      {status === "loading" && (
+        <BannerVideo
+          text="home"
+          pageVideo={776443340}
+          overlayOpacity={0.1}
+          overlayColor={COLOR_BASE}
+          loopEndTime={loopEndTime}
+        />
+      )}
+      {status === "success" && (
+        <>
+          <BannerVideo
+            text="home"
+            pageVideo={776443340}
+            overlayOpacity={0.1}
+            overlayColor={COLOR_BASE}
+            loopEndTime={loopEndTime}
+          />
+          <ChartHome
+            columnCount={7}
+            data={dataHome}
+            loopEndTime={loopEndTime}
+            backgroundColor={1}
+          />
+          <RowButton
+            buttonTo="/practice_areas"
+            buttonText="What we do"
+            backgroundColor={4}
+          />
+          <OrgsCallout />
+        </>
+      )}
     </>
   );
 };
