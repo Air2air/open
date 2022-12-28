7a51ea6801c333adce743f2eb1682dd63000b1ef
Air2air <todd@dunningmarketing.com>
2022-12-27 22:19:32 -0800

    Layout

--- src/pages/CaseStudies/pageCaseStudies.tsx
+++ src/pages/CaseStudies/pageCaseStudies.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import BannerVideo from "components/BannerVideo/bannerVideo";
 import { COLOR_BASE } from "constants/index";
 import Callout from "components/Callout/callOut";
-import ChartCaseStudies from "components/Chart/CaseStudies/chartCaseStudies";
+// import ChartCaseStudies from "components/Chart/CaseStudies/chartCaseStudies";
 
 const dataSource = "data/dataCaseStudies.json";
 
