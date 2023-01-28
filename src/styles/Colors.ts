4030b4b199067be336391fdb94cfc5025a5222cd
Air2air <todd@dunningmarketing.com>
2023-01-28 11:32:57 -0800

    Responsive

--- src/styles/Colors.ts
+++ src/styles/Colors.ts
@@ -1,5 +1,5 @@
-const BASEHUE = 200;
-const SATURATION = 60;
+const BASEHUE = 205;
+const SATURATION = 97;
 
 export const COLOR_BASE = `hsl(${BASEHUE}, ${SATURATION}%, 50%)`;
 export const COLOR_BLACK = `hsl(0, 0%, 0%)`;
@@ -13,9 +13,9 @@ export const COLOR_BASE_4 = `hsl(${BASEHUE}, ${SATURATION}%, 50%)`;
 
 // SITE
 export const COLOR_BACKGROUND = `hsl(${BASEHUE}, ${SATURATION}%, 90%)`;
-export const COLOR_HEADER = `hsl(${BASEHUE}, ${SATURATION}%, 60%)`;
+export const COLOR_HEADER = `hsl(${BASEHUE}, ${SATURATION}%, 50%)`;
 export const COLOR_HEADER_HOVER = `hsl(${BASEHUE}, ${SATURATION}%, 60%)`;
-export const COLOR_FOOTER = `hsl(${BASEHUE}, ${SATURATION}%, 60%)`;
+export const COLOR_FOOTER = `hsl(${BASEHUE}, ${SATURATION}%, 50%)`;
 
 // BUTTONS
 export const COLOR_BUTTON_TEXT = COLOR_WHITE;
