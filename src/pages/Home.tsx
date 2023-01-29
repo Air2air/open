3f43fce25677e08848acc66267d6a4366fa528f9
Air2air <todd@dunningmarketing.com>
2023-01-28 16:38:23 -0800

    Responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import OrgsCallout from "components/OrgsCallout/orgsCallOut";
+// import OrgsCallout from "components/OrgsCallout/orgsCallOut";
 import Section from "components/Section/section";
 
 const HomePage = () => {
   return (
     <>
       <Section jsonFile="/data/homeContent.json" />
-      <OrgsCallout jsonFile="/data/orgsContent.json" />
+      {/* <OrgsCallout jsonFile="/data/orgsContent.json" /> */}
     </>
   );
 };
