034d320ae5097a34694a1e09e2824b60959be81d
Air2air <todd@dunningmarketing.com>
2023-02-14 16:01:03 -0800

    Layout

--- src/components/Callout/calloutComponents.tsx
+++ src/components/Callout/calloutComponents.tsx
@@ -1,5 +1,6 @@
 import {
   BREAKPOINT,
+  COLOR_TEXT,
   CONTENT_WIDTH_DESKTOP,
   CONTENT_WIDTH_MOBILE,
   FONT_BANNER_HEADING_DESKTOP,
@@ -15,7 +16,6 @@ import Container from "components/Container/container";
 const CALLOUT_HEIGHT = HEADER_HEIGHT + FOOTER_HEIGHT;
 
 export const CallOutOuter = styled.div`
-
   min-height: calc(100vh - ${CALLOUT_HEIGHT}px);
   width: 100%;
 `;
@@ -23,13 +23,17 @@ export const CallOutOuter = styled.div`
 export const CallOutInner = styled.div`
   margin: 0 auto;
   min-height: calc(100vh - ${CALLOUT_HEIGHT}px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   @media (min-width: ${BREAKPOINT}px) {
     width: ${CONTENT_WIDTH_DESKTOP};
   }
   @media (max-width: ${BREAKPOINT}px) {
     width: ${CONTENT_WIDTH_MOBILE};
   }
-  background: red;
+  /* background: red; */
 `;
 
 export const CalloutSkeleton = (props) => (
@@ -40,23 +44,30 @@ export const CalloutSkeleton = (props) => (
 
 export const VariantTitle = styled.div`
   text-align: center;
-  height: auto;
+  justify-content: center;
+  align-items: center;
+  color: ${COLOR_TEXT};
   @media (min-width: ${BREAKPOINT}px) {
-    ${FONT_BANNER_HEADING_DESKTOP}
+    height: 100px;
+    ${FONT_BANNER_HEADING_DESKTOP};
   }
   @media (max-width: ${BREAKPOINT}px) {
-    ${FONT_BANNER_HEADING_MOBILE}
+    height: 100px;
+    ${FONT_BANNER_HEADING_MOBILE};
+    color: #ccc;
   }
   /* background: green; */
 `;
 
 export const VariantText = styled.div`
   height: auto;
+
   @media (min-width: ${BREAKPOINT}px) {
     ${FONT_BANNER_SUBHEAD_DESKTOP}
   }
   @media (max-width: ${BREAKPOINT}px) {
     ${FONT_BANNER_SUBHEAD_MOBILE}
   }
+  color: ${COLOR_TEXT};
   /* background: red; */
 `;
